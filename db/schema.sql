21f46fb29f656e61faeb560ab3e1ccce0a891e96
AliLotfi123 <a.lotfi@outlook.com>
2020-11-04 15:33:39 +0100

    setup docker with mysql

---
+++ db/schema.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE IF NOT EXISTS blog;
+
+USE blog;
+
+CREATE TABLE IF NOT EXISTS post (
+  id INT UNSIGNED AUTO_INCREMENT,
+  title VARCHAR(255) NOT NULL,
+  content TEXT NOT NULL,
+  PRIMARY KEY (id)
+);
+
+INSERT INTO posts (id, title, content) 
+VALUES (1, 'using the serverless framework with AWS', 'This is the content of the the test post')
