6bf81521e358b8f9cbf23bd2d200fe6a8b9ff2dc
AliLotfi123 <a.lotfi@outlook.com>
2020-11-05 09:44:30 +0100

    setup of POST for db

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,10 +1,21 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import mysql from "mysql";
 import db from "../../lib/db";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const posts = await db.query("SELECT * FROM post");
-  await db.end();
-
-  res.statusCode = 200;
-  res.json(posts);
+  if (req.method === "GET") {
+    const posts = await db.query("SELECT * FROM post");
+    await db.end();
+    res.statusCode = 200;
+    res.json(posts);
+  } else if (req.method === "POST") {
+    const body: { title: string; content: string } = req.body;
+    const result = await db.query<mysql.OkPacket>(
+      "INSERT INTO post (title, content) VALUES (?, ?)",
+      [body.title, body.content]
+    );
+    await db.end();
+    res.statusCode = 200;
+    res.json({ id: result.insertId, title: body.title, conten: body.content });
+  }
 };
